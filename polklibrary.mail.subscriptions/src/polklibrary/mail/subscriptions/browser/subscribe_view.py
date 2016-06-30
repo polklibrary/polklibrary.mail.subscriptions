190d8902164f67f7760d1d1957fd33c5c0339180
hietpasd <hietpasd@uwosh.edu>
2016-06-30 13:31:48 -0500

    fixed bugs

--- polklibrary.mail.subscriptions/src/polklibrary/mail/subscriptions/browser/subscribe_view.py
+++ polklibrary.mail.subscriptions/src/polklibrary/mail/subscriptions/browser/subscribe_view.py
@@ -9,17 +9,31 @@ class SubscribeView(BrowserView):
     template = ViewPageTemplateFile("subscribe_view.pt")
     
     email = ''
+    subscribed = False
+    unsubscribed = False
     
     def __call__(self):
         self.email = ''
+        self.subscribed = False
+        self.unsubscribed = False
+    
+    
     
         if 'form.subscriber.submit' in self.request.form:
             email = self.request.form.get('form.subscriber.email','').strip()
             match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
 
             if match != None:
-                self.context.subscribers += email + '\n'
+                
+                if self.context.subscribers:
+                    self.context.subscribers += email + '\n'
+                else:
+                    self.context.subscribers = email + '\n'
+                    
+                self.subscribed = True
                 self.email = email
+                
+                
 
         if 'form.subscriber.cancel' in self.request.form:
             email = self.request.form.get('form.subscriber.email','').strip()
@@ -28,6 +42,7 @@ class SubscribeView(BrowserView):
             if match != None:
                 target = email + '\n'
                 self.context.subscribers = self.context.subscribers.replace(target, '')
+                self.unsubscribed = True
                 self.email = email
     
         return self.template()
